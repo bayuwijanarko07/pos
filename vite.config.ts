e89335e39a88d888d5ce690aca15dff7fb93e5f0
bayuwijanarko07 <bayuwijanarko07@gmail.com>
2026-02-20 18:05:14 +0700

    chore: config autoimports icons iconsresolver

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
+import AutoImport from 'unplugin-auto-import/vite'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
-  plugins: [vue(), tailwindcss(),],
+  plugins: [
+    vue(), 
+    tailwindcss(),
+    AutoImport({
+      resolvers: [IconsResolver()],
+    }),
+    Icons(),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
