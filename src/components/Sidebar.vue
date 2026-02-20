89d0fef66b571a083e79b50cf0c0f19552e01a42
bayuwijanarko07 <bayuwijanarko07@gmail.com>
2026-02-21 01:51:31 +0700

    style: add card dashboard ui

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,13 +1,14 @@
 <template>
     <aside :class="[
-        'fixed top-0 left-0 z-50 h-screen w-30 bg-white text-gray-800',
+        'fixed flex flex-col top-0 left-0 z-50 h-screen w-30 bg-white text-gray-800',
     ]">
         <div class="flex items-center justify-center p-4">
             <h1 class="text-center font-bold">POS</h1>
         </div>
+
        <nav class="p-4 space-y-2">
             <router-link
-                v-for="menu in menus"
+                v-for="menu in topMenus"
                 :key="menu.path"
                 :to="menu.path"
                 class="flex justify-center items-center rounded-lg p-5"
@@ -18,6 +19,20 @@
                 <Icon :icon="menu.icon" class="text-xl"/>
             </router-link>
         </nav>
+
+        <nav class="p-4 space-y-2 mt-auto">
+            <router-link
+                v-for="menu in bottomMenus"
+                :key="menu.path"
+                :to="menu.path"
+                class="flex justify-center items-center rounded-lg p-5"
+                :class="route.path === menu.path 
+                ? 'bg-blue-400 text-white' 
+                : 'text-gray-600'"
+            >
+                <Icon :icon="menu.icon" class="text-xl" />
+            </router-link>
+        </nav>
     </aside>
 </template>
 <script setup lang="ts">
@@ -25,8 +40,18 @@
     const route = useRoute()
 
     const menus = [
-        { name: 'Dashboard', path: '/', icon: "mdi:view-dashboard" },
-        { name: 'Orders', path: '/orders', icon: "mdi:order-bool-descending-variant" },
-        { name: 'Products', path: '/products', icon: "mdi:material" },
+        { label: 'Dashboard', path: '/', icon: "mdi:view-dashboard" },
+        { label: 'Orders', path: '/orders', icon: "mdi:order-bool-descending-variant" },
+        { label: 'Products', path: '/products', icon: "mdi:material" },
+        { label: 'User', path: '/user', icon: "mdi:user" },
+        { label: 'Settings', path: '/settings', icon: "mdi:settings" },
     ]
+
+    const bottomMenus = computed(() =>
+        menus.filter(menu => ['/user', '/settings'].includes(menu.path))
+    )
+
+    const topMenus = computed(() =>
+        menus.filter(menu => !['/user', '/settings'].includes(menu.path))
+    )
 </script>
--- src/pages/DashboardPage.vue
+++ src/pages/DashboardPage.vue
@@ -1,16 +1,71 @@
 <template>
-  <div class="grid sm:grid-cols-1 lg:grid-cols-3 xl:grid-cols-4 gap-4 flex-1 overflow-y-auto">
+  <div class="grid sm:grid-cols-1 lg:grid-cols-3 xl:grid-cols-3 gap-4 flex-1 overflow-y-auto">
     <Card class="p-5 md:p-6">
 
-      <div class="flex items-center justify-between mb-6">
+      <div class="flex items-center justify-between pb-10">
         <div class="flex items-center gap-4">
-          <div class="flex items-center justify-center w-12 h-12 bg-gray-100 rounded">
+          <div class="flex items-center justify-center w-15 h-15 bg-green-100 rounded">
             <Icon icon="mdi:dollar" class="text-green-500 text-2xl" />
           </div>
           <h5 class="text-gray-600">Total Revenue</h5>
         </div>
 
-        <button class="w-6 h-6 flex items-center justify-center rounded hover:bg-gray-200 cursor-pointer">
+        <button class="w-10 h-10 flex items-center justify-center rounded hover:bg-gray-200 cursor-pointer">
+          <Icon icon="mdi:dots-vertical" class="text-gray-600" />
+        </button>
+      </div>
+
+      <div class="flex items-center justify-between">
+        <div class="flex items-center">
+          <Icon icon="mdi:dollar" class="text-green-500 text-3xl" />
+          <h1 class="text-3xl">500000</h1>
+        </div>
+
+        <span class="flex items-center gap-1 rounded-full bg-green-50 px-2.5 py-0.5 text-sm font-medium text-green-600">
+          <Icon icon="mdi:arrow-down" class="text-red-500" />
+          <span class="text-gray-600">100%</span>
+        </span>
+      </div>
+    </Card>
+
+    <Card class="p-5 md:p-6">
+
+      <div class="flex items-center justify-between pb-10">
+        <div class="flex items-center gap-4">
+          <div class="flex items-center justify-center w-15 h-15 bg-yellow-100 rounded">
+            <Icon icon="mdi:text-box-edit-outline" class="text-yellow-500 text-2xl" />
+          </div>
+          <h5 class="text-gray-600">Total Order</h5>
+        </div>
+
+        <button class="w-10 h-10 flex items-center justify-center rounded hover:bg-gray-200 cursor-pointer">
+          <Icon icon="mdi:dots-vertical" class="text-gray-600" />
+        </button>
+      </div>
+
+      <div class="flex items-center justify-between">
+        <div class="flex items-center gap-2">
+          <h1 class="text-3xl">4321</h1>
+        </div>
+
+        <span class="flex items-center gap-1 rounded-full bg-green-50 px-2.5 py-0.5 text-sm font-medium text-green-600">
+          <Icon icon="mdi:arrow-up" class="text-green-500" />
+          <span class="text-gray-600">100%</span>
+        </span>
+      </div>
+    </Card>
+
+    <Card class="p-5 md:p-6">
+
+      <div class="flex items-center justify-between pb-10">
+        <div class="flex items-center gap-4">
+          <div class="flex items-center justify-center w-15 h-15 bg-gray-100 rounded">
+            <Icon icon="mdi:store-clock-outline" class="text-gray-500 text-2xl" />
+          </div>
+          <h5 class="text-gray-600">Average Sell</h5>
+        </div>
+
+        <button class="w-10 h-10 flex items-center justify-center rounded hover:bg-gray-200 cursor-pointer">
           <Icon icon="mdi:dots-vertical" class="text-gray-600" />
         </button>
       </div>
@@ -18,7 +73,7 @@
       <div class="flex items-center justify-between">
         <div class="flex items-center gap-2">
           <Icon icon="mdi:dollar" class="text-green-500 text-3xl" />
-          <h1 class="text-3xl">500000</h1>
+          <h1 class="text-3xl">500</h1>
         </div>
 
         <span class="flex items-center gap-1 rounded-full bg-green-50 px-2.5 py-0.5 text-sm font-medium text-green-600">
